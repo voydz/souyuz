3459e75f7199a4d7aa15bb78458529fec4bbdcd5
Felix Rudat <frudat@doegel.de>
2017-05-09 11:09:09 +0200

    bumping version to 0.6.7

--- fastlane-plugin-souyuz/lib/fastlane/plugin/souyuz/version.rb
+++ fastlane-plugin-souyuz/lib/fastlane/plugin/souyuz/version.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 module Fastlane
   module Souyuz
-    VERSION = "0.6.6"
+    VERSION = "0.6.7"
   end
 end
--- lib/souyuz/version.rb
+++ lib/souyuz/version.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
 module Souyuz
-  VERSION = "0.6.6"
+  VERSION = "0.6.7"
   DESCRIPTION = "A fastlane component to make Xamarin builds a breeze"
 end
